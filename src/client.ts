e97f2c68d6f2dc977660d96eeaa6034bfac23851
Build Uploader <worldisblack.1@gmail.com>
2021-05-22 22:56:07 +0900

    Initial commit

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import Discord from 'discord.js';
 import fs from 'fs';
+import yargs from 'yargs';
 import Core from "./core";
 import Files from "./files";
 
@@ -8,7 +9,7 @@ class Client {
 
     public constructor() {
         const client = new Discord.Client();
-        client.login('NzcwMTA0NTc1OTI2NzMwNzcy.X5Ytxg.BBFfH3rNoNmj6khHUFMfs9ClRCk');
+        client.login('token');
 
         Core.Events.Manager.loadEvents(`events`);
         Core.Events.Manager.registerEvents(client);
